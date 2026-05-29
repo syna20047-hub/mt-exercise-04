ba884cfdd613bca77ca6f7b4f346f194209d9114
syna20047-hub <syna20047@gmail.com>
2026-05-29 17:43:39 +0200

    Improve beam plot axis labels

--- scripts/plot_beam_results.py
+++ scripts/plot_beam_results.py
@@ -3,19 +3,23 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("beam_results.csv")
 
+# Graph 1: Beam size vs BLEU
 plt.figure()
 plt.plot(df["beam_size"], df["bleu"], marker="o")
 plt.xlabel("Beam size")
 plt.ylabel("BLEU score")
 plt.title("Effect of Beam Size on BLEU")
+plt.xticks(df["beam_size"])
 plt.grid(True)
 plt.savefig("beam_size_vs_bleu.png", dpi=300, bbox_inches="tight")
 
+# Graph 2: Beam size vs generation time
 plt.figure()
 plt.plot(df["beam_size"], df["time_seconds"], marker="o")
 plt.xlabel("Beam size")
 plt.ylabel("Time in seconds")
 plt.title("Effect of Beam Size on Generation Time")
+plt.xticks(df["beam_size"])
 plt.grid(True)
 plt.savefig("beam_size_vs_time.png", dpi=300, bbox_inches="tight")
 
